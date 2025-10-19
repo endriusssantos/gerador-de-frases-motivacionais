e3795d38ca49b8cf9b8ae59a8dfc3eff333b2216
Endrius <endriussilvasantos@gmail.com>
2025-10-19 20:28:53 -0300

    criado jsx principal e configurado estilos css iniciais

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
-    <div>
-      
+    <div className="container">
+      <h1>Gerador de Frase Motivacional</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, est
+        ullam. Earum nisi, sed error ex vero assumenda corporis odit dolore
+        eligendi nihil, enim at, corrupti doloribus dolorem ullam a!
+      </p>
+      <button>Gerar Frase</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
